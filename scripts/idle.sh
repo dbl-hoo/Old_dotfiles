4a052f4e439f72f80a6c09947781f528c3a45ebf
Jason Kirkham <jason.kirkham@gmail.com>
2023-11-05 18:11:58 -0500

    fixed lockscreen

--- scripts/idle.sh
+++
@@ -1,5 +0,0 @@
-swayidle \
-	timeout 60 '~/dotfiles/scripts/lock.sh --grace 10 --fade-in 4' \
-	timeout 80 'swaymsg "output * dpms off"' \
-	resume 'swaymsg "output * dpms on"' \
-	before-sleep '~/dotfiles/scripts/lock.sh'
