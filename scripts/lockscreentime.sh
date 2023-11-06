49b94b3be25d2106e2674541ed00a0a27cd53f8a
Jason Kirkham <jason.kirkham@gmail.com>
2023-11-06 14:01:50 -0500

    updated autostart and lockscreen

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -13,7 +13,7 @@ timeswaylock=600
 timeoff=660
 
 if [ -f "/usr/bin/swayidle" ]; then
-    echo "swayidle is installed."
+   echo "swayidle is installed."
     swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
 else
     echo "swayidle not installed."
